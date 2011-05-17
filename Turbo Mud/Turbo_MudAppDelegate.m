258be8f78a07dfab5153f7fcbff8091608f23e26
Samuel Goodwin <goodwinlabs@gmail.com>
2011-05-16 23:40:11 -0400

    And of course that's useless.

--- Turbo Mud/Turbo_MudAppDelegate.m
+++ Turbo Mud/Turbo_MudAppDelegate.m
@@ -13,17 +13,7 @@
 @synthesize window;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
-    __block int count = 0;
     
-    dispatch_source_t timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER,0, 0, dispatch_get_main_queue());
-    if (timer){
-        dispatch_source_set_timer(timer, dispatch_walltime(NULL, 0), 10000000000, 1);
-        dispatch_source_set_event_handler(timer, ^{
-            count++;
-            NSLog(@"Tick %i, %@", count, [NSDate date]);
-        });
-        dispatch_resume(timer);
-    }
 }
 
 @end
