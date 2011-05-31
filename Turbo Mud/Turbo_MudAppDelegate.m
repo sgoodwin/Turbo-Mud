4aab166141c8be74a0b66ede3521e3b6ca7b8972
Samuel Goodwin <goodwinlabs@gmail.com>
2011-05-31 02:08:29 -0400

    Got proper color stripping and coloring working as well as identifying a server command I wasn't properly receiving.

--- Turbo Mud/Turbo_MudAppDelegate.m
+++ Turbo Mud/Turbo_MudAppDelegate.m
@@ -17,11 +17,15 @@
 #import "NSMutableArray+FIFO.h"
 #import "NSString+ASCIIDebug.h"
 
+const CGFloat kFontSize = 12.0f;
 const unichar iac = 255;
 
 @interface Turbo_MudAppDelegate()
 - (void)scrollToBottom:(id)sender;
-- (NSAttributedString *)processIncomingStream:(NSString*)string;
+- (NSAttributedString*)processIncomingStream:(NSString*)string withPreviousAttributes:(NSMutableDictionary **)previousAttributes;
+- (id)attributeForCode:(NSInteger)code;
+- (NSString*)attributeNameForCode:(NSInteger)code;
+- (void)setupDefaults:(NSMutableDictionary*)dict overwrite:(BOOL)overwrite;
 @end
 
 @implementation Turbo_MudAppDelegate
@@ -29,6 +33,10 @@ const unichar iac = 255;
 @synthesize window, textField, scrollView, inputQueue;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
+    NSString *string = @"Enter an option or enter your character's name. ÿù";
+    for(NSInteger i = 0;i<[string length];i++){
+        NSLog(@"char %lu is %i", i, [string characterAtIndex:i]);
+    }
     self.inputQueue = [NSMutableArray arrayWithCapacity:2];
     
     int fd;
@@ -78,14 +86,18 @@ const unichar iac = 255;
         }
         
         NSString *results = [NSString stringWithCString:buffer encoding:NSASCIIStringEncoding];
-        Turbo_MudAppDelegate *weakSelf = self;
-        dispatch_async(dispatch_get_main_queue(), ^(void) {
-            [results enumerateLinesUsingBlock:^(NSString *line, BOOL *stop) {
-                NSAttributedString *processedLine = [weakSelf processIncomingStream:line];
-                [[weakSelf.textField textStorage] appendAttributedString:processedLine];
-                [weakSelf scrollToBottom:nil];
-            }];
-        });
+        NSArray *lines = [results componentsSeparatedByString:@"\r\n"];
+        NSMutableDictionary *previousAttributes = [NSMutableDictionary dictionaryWithCapacity:3];
+        for(NSString *line in lines){
+            NSAttributedString *processedLine = [self processIncomingStream:line withPreviousAttributes:&previousAttributes];
+            if(processedLine && [processedLine length] > 0){
+                dispatch_async(dispatch_get_main_queue(), ^(void) {
+                    [[self.textField textStorage] appendAttributedString:processedLine];
+                    [self scrollToBottom:nil];
+                });
+            }
+        }
+
     });
     dispatch_source_set_cancel_handler(readSource, ^{ 
         NSLog(@"Cancelling Reading handler");
@@ -100,7 +112,6 @@ const unichar iac = 255;
         if(!nextInputLine){
             return;
         }
-        NSLog(@"Queue has %@ left", self.inputQueue);
         size_t numberOfBytesToWrite = [nextInputLine length];
         const char *bytesToWrite = [[nextInputLine stringByAppendingString:@"\r\n"] cStringUsingEncoding:NSASCIIStringEncoding];
         size_t numberOfBytesActuallyWritten = write(fd, bytesToWrite, numberOfBytesToWrite);
@@ -123,15 +134,16 @@ const unichar iac = 255;
     [self.inputQueue enqueue:[sender stringValue]];
 }
 
-- (NSAttributedString*)processIncomingStream:(NSString*)string{
+- (NSAttributedString*)processIncomingStream:(NSString*)string withPreviousAttributes:(NSMutableDictionary **)previousAttributes{   
+    [self setupDefaults:*previousAttributes overwrite:NO];
     if([string length] == 0){
         NSMutableAttributedString *attributedResult = [[NSMutableAttributedString alloc] init];
-        [attributedResult addAttribute:NSForegroundColorAttributeName value:[NSColor whiteColor] range:NSMakeRange(0, [string length])];
-        return attributedResult;
+        [attributedResult addAttributes:*previousAttributes range:NSMakeRange(0, [string length])];
+        return [attributedResult autorelease];
     }
     
     NSError *error = nil;
-    NSRegularExpression *serverCommandRegex = [NSRegularExpression regularExpressionWithPattern:@"\\xFF(..)" options:NSRegularExpressionUseUnixLineSeparators error:&error];
+    NSRegularExpression *serverCommandRegex = [NSRegularExpression regularExpressionWithPattern:@"\\xFF(..?)" options:NSRegularExpressionUseUnixLineSeparators error:&error];
     if(error){
         NSLog(@"error: %@", [error localizedDescription]);
     }
@@ -139,8 +151,9 @@ const unichar iac = 255;
         if(!result){
             return;
         }
-        //NSRange rangeToHandle = NSMakeRange([result rangeAtIndex:1].location-1, [result rangeAtIndex:1].length+1);
-        //NSString *subString = [string substringWithRange:rangeToHandle];
+        NSRange rangeToHandle = NSMakeRange([result rangeAtIndex:1].location-1, [result rangeAtIndex:1].length+1);
+        NSString *subString = [string substringWithRange:rangeToHandle];
+        NSLog(@"Char code of commands: %@", [subString charCodeString]);
         // respond according to the matched server message;
     }];
     NSString *commandStrippedString = [serverCommandRegex stringByReplacingMatchesInString:string options:NSRegularExpressionUseUnixLineSeparators range:NSMakeRange(0,[string length]) withTemplate:@""];
@@ -151,26 +164,55 @@ const unichar iac = 255;
     if(error){
         NSLog(@"error: %@", [error localizedDescription]);
     }
-    [colorRegex enumerateMatchesInString:commandStrippedString options:NSMatchingReportProgress range:NSMakeRange(0, [commandStrippedString length]) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
-        if(!result){
-            return;
-        }
-        NSLog(@"original string: %@", commandStrippedString);
-        NSRange firstRange = [result rangeAtIndex:1];
-        if(firstRange.location != NSNotFound){
-            
-        }
-        
-        NSRange secondRange = [result rangeAtIndex:2];
-        if(secondRange.location != NSNotFound){
-           
-        }
-    }];
-    
+    NSArray *matches = [colorRegex matchesInString:commandStrippedString options:NSMatchingReportProgress range:NSMakeRange(0, [commandStrippedString length])];
     NSString *colorStrippedString = [colorRegex stringByReplacingMatchesInString:commandStrippedString options:NSRegularExpressionUseUnixLineSeparators range:NSMakeRange(0,[commandStrippedString length]) withTemplate:@""];
     NSMutableAttributedString *attributedResult = [[NSMutableAttributedString alloc] initWithString:[colorStrippedString stringByAppendingString:@"\r\n"]];
-    [attributedResult addAttribute:NSForegroundColorAttributeName value:[NSColor whiteColor] range:NSMakeRange(0, [colorStrippedString length])];
-    return attributedResult;
+    [attributedResult addAttributes:*previousAttributes range:NSMakeRange(0, [colorStrippedString length])];
+    
+    NSInteger offset = 2;
+    for(int i=0;i<[matches count];i++){
+        NSTextCheckingResult *result = [matches objectAtIndex:i];
+        NSRange firstRange = [result rangeAtIndex:1];
+        if(firstRange.location != NSNotFound){
+            NSString *firstString = [commandStrippedString substringWithRange:firstRange];
+            firstRange.location = firstRange.location - offset;
+            NSInteger code = [firstString integerValue];
+            if(code == 0){
+                [self setupDefaults:*previousAttributes overwrite:YES];
+            }else{
+                [*previousAttributes setObject:[self attributeForCode:code] forKey:[self attributeNameForCode:code]];
+            }
+            [attributedResult addAttributes:*previousAttributes range:NSMakeRange(firstRange.location,[attributedResult length]-firstRange.location)];
+            offset+=2;
+        }
+        NSRange secondRange = [result rangeAtIndex:2];
+        if(secondRange.location != NSNotFound){
+            NSString *secondString = [commandStrippedString substringWithRange:secondRange];
+            secondRange.location = secondRange.location - offset;
+            NSInteger code = [secondString integerValue];
+            if(code == 0){
+                [self setupDefaults:*previousAttributes overwrite:YES];
+            }else{
+                [*previousAttributes setObject:[self attributeForCode:code] forKey:[self attributeNameForCode:code]];
+            }
+            [attributedResult addAttributes:*previousAttributes range:NSMakeRange(secondRange.location,[attributedResult length]-secondRange.location)];
+            offset+=5;
+        }
+    }
+    return [attributedResult autorelease];
+}
+
+- (void)setupDefaults:(NSMutableDictionary*)dict overwrite:(BOOL)overwrite{
+    // Setup defaults...
+    if(nil == [dict objectForKey:NSForegroundColorAttributeName] || overwrite){
+        [dict setObject:[NSColor whiteColor] forKey:NSForegroundColorAttributeName];
+    }
+    if(nil == [dict objectForKey:NSBackgroundColorAttributeName] || overwrite){
+        [dict setObject:[NSColor blackColor] forKey:NSBackgroundColorAttributeName];
+    }
+    if(nil == [dict objectForKey:NSFontAttributeName] || overwrite){
+        [dict setObject:[NSFont fontWithName:@"Menlo" size:kFontSize] forKey:NSFontAttributeName];
+    }
 }
 
 - (void)scrollToBottom:(id)sender{
@@ -182,8 +224,59 @@ const unichar iac = 255;
     } else {
         newScrollOrigin=NSMakePoint(0.0,0.0);
     }
-    
     [[self.scrollView documentView] scrollPoint:newScrollOrigin];
     
+}
+
+
+/* color codes taken from http://pueblo.sourceforge.net/doc/manual/ansi_color_codes.html */
+- (NSString*)attributeNameForCode:(NSInteger)code{
+    if(code == 1){
+        return NSFontAttributeName;
+    }
+    if(code >= 2 && code <=29){
+        return nil;
+    }
+    if(code >= 30 && code <= 39){
+        return NSForegroundColorAttributeName;
+    }
+    return NSBackgroundColorAttributeName;
+}
+
+- (id)attributeForCode:(NSInteger)code{
+    switch(code){
+        case 1:
+            return [NSFont fontWithName:@"Menlo Bold" size:kFontSize];
+        case 30:
+            return [NSColor blackColor];
+            break;
+        case 31:
+            return [NSColor redColor];
+            break;
+        case 32:
+            return [NSColor greenColor];
+            break;
+        case 33:
+            return [NSColor yellowColor];
+            break;
+        case 34:
+            return [NSColor blueColor];
+            break;
+        case 35:
+            return [NSColor magentaColor];
+            break;
+        case 36:
+            return [NSColor cyanColor];
+            break;
+        case 37:
+            return [NSColor whiteColor];
+            break;
+        case 39:
+            return [NSColor whiteColor];
+            break;
+        default:
+            return [NSColor whiteColor];
+            break;
+    }
 }
 @end
